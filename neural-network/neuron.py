568ba733d2401f14d1d0c1879b45a38fe671b5c9
Rohit <vadlamudi.rohit5@gmail.com>
2025-05-05 12:19:29 +0530

    turn the neuron to a basic layer output

--- neural-network/neuron.py
+++ neural-network/neuron.py
@@ -1,9 +1,17 @@
 inputs = [1.2, 5.7, 9.2] # Ouput from each neuron in previous layer
 # Every unique input has a unique weight associated with it
-weights = [2.2, 4.1, 3.3]
+n1weights = [2.2, 4.1, 3.3]
+n2weights = [1.2, 3.2, 4.7]
+n3weights = [5.1, 4.6, 7.3]
+
 # every unique neuron has a unique bias too
-bias = 3
+n1bias = 3
+n2bias = 0.2
+n3bias = 1.5
 
 # Step 1: Neuron to add up all the inputs times the weights + bias
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+output = [inputs[0] * n1weights[0] + inputs[1] * n1weights[1] + inputs[2] * n1weights[2] + n1bias,
+          inputs[0] * n2weights[0] + inputs[1] * n2weights[1] + inputs[2] * n2weights[2] + n2bias,
+          inputs[0] * n3weights[0] + inputs[1] * n3weights[1] + inputs[2] * n3weights[2] + n3bias
+        ]
 print(output)
